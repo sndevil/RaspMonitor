a73de6df3dcd388efc7912fb24e4f73ec5cce982
sndevil <me@me.com>
2016-03-01 16:16:46 +0330

    some changes. add actions to form

--- source/main_bckup.c
+++ source/main_bckup.c
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
     GtkBuilder      *builder; 
     GtkWidget       *window;
+    GtkWidget       *notebook;
+    GtkWidget       *table;
 
     gtk_init(&argc, &argv);
 
@@ -13,6 +15,11 @@ int main(int argc, char *argv[])
     gtk_builder_add_from_file (builder, "visual/Satmonitor.glade", NULL);
     window = GTK_WIDGET(gtk_builder_get_object(builder, "Mainwindow"));
     gtk_builder_connect_signals(builder, NULL);
+
+    notebook = gtk_notebook_new ();
+    gtk_notebook_set_tab_pos (GTK_NOTEBOOK (notebook), GTK_POS_TOP);
+    gtk_table_attach_defaults(GTK_TABLE(table), notebook, 0,6,0,1);
+    gtk_widget_show(notebook);
 
     g_object_unref(builder);
 
